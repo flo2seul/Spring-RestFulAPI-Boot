a1e482e789eeb8ebf70e795c38627c6dc4103960
flo2seul <seulaayoon@gmail.com>
2021-06-26 16:00:02 +0900

    find_result.jsp로 안넘어감

--- sp05_Products/src/main/java/com/encore/spring/controller/ProductController.java
+++ sp05_Products/src/main/java/com/encore/spring/controller/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,36 +12,54 @@ import com.encore.spring.model.ProductCatalog;
 
 @Controller
 public class ProductController {
-
-	@Autowired
-	private ProductCatalog productCatalog;
 	
+	@Autowired
+	private ProductCatalog myProductCatalog;
+	
+//	@RequestMapping(value="myProduct.do", method=RequestMethod.POST)
 	@RequestMapping("myProduct.do")
 	public ModelAndView insert(Product pvo) throws Exception{
-		System.out.println("폼으로부터 받은 데이터"+pvo.getId());
 		
-		productCatalog.addProduct(pvo);
+		System.out.println("폼으로부터 받은 데이타 "+pvo.getId()); //0
 		
-		System.out.println("폼으로부터 받은 데이터"+pvo.getId());
+		myProductCatalog.addProduct(pvo); //여기서 dao, sqlSession, 디비 로 가서 id 값이 자동증가...--> 그 값을 다시 vo에 set해주기 떄문에
 		
-		return new ModelAndView("insert_result","info",pvo);
+		System.out.println("폼으로부터 받은 데이타 "+pvo.getId()); //6,7...
+		return new ModelAndView("insert_result", "info", pvo);
 	}
 	
-	@RequestMapping("myProductSerch.do")
+	@RequestMapping("myProductSearch.do")
 	public ModelAndView getProducts(String word, String command) throws Exception{
 		String viewName = "";
 		List<Product> list = null;
 		
 		if(command.equals("findByProductName")) {
-			list = productCatalog.findByProductName(word);
-			viewName = "find_result";
-			
+			list = myProductCatalog.findByProductName(word);
+			viewName=  "find_result";
 		}else if(command.equals("findByProductMaker")) {
-			list = productCatalog.findByProductMaker(word);
-			viewName = "find_result";
-			
+			list = myProductCatalog.findByProductMaker(word);
+			viewName=  "find_result";
 		}
-		return new ModelAndView(viewName, "list",list);
+		return new ModelAndView(viewName, "list", list);		
 	}
-	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
