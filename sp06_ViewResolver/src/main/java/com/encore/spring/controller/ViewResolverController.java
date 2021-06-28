e189b6199ae7efa9c6f462f3739067950d639f3d
flo2seul <seulaayoon@gmail.com>
2021-06-28 11:31:39 +0900

    BeanNameViewResolver

--- sp06_ViewResolver/src/main/java/com/encore/spring/controller/ViewResolverController.java
+++ sp06_ViewResolver/src/main/java/com/encore/spring/controller/ViewResolverController.java
@@ -12,4 +12,19 @@ public class ViewResolverController {
 		System.out.println("register() calling...");
 		return new ModelAndView("register_result","info","InternalResourceViewResolver...");
 	}
+	
+	//BeanNameViewResolver와 컨트롤러가 어떻게 연결되는지,,,
+	
+	@RequestMapping("findBoard.do")
+	public ModelAndView findBoard() {
+		System.out.println("findBoard calling...");
+		return new ModelAndView("board_result"); //board/result/find_ok.jsp
+	}
+	
+	@RequestMapping("findProduct.do")
+	public ModelAndView findProduct() {
+		System.out.println("findProduct calling...");
+		return new ModelAndView("product_result"); //product/result/find_ok.jsp
+	}
+	
 }
