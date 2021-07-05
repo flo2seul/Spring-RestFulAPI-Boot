bd344539ba660c9bd93109f43ccf5eec14543012
flo2seul <seulaayoon@gmail.com>
2021-07-05 15:25:50 +0900

    restful practice

---
+++ sp13_SpringRestfulAPI/src/main/java/com/encore/rest/dto/Greeting.java
@@ -0,0 +1,34 @@
+package com.encore.rest.dto;
+
+public class Greeting {
+	private long id;
+	private String content;
+	public Greeting() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "Greeting [id=" + id + ", content=" + content + "]";
+	}
+	
+	
+
+}
---
+++ sp14_Restful_BookTest/src/main/java/com/encore/rest/controller/BookRestController.java
@@ -0,0 +1,67 @@
+package com.encore.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.encore.rest.service.BookService;
+import com.encore.rest.vo.Book;
+
+@RestController
+@RequestMapping("api")
+public class BookRestController {
+
+	@Autowired
+	private BookService bookService;
+	
+	//http://127.0.0.1:8888/rest/api/book
+	@GetMapping("book")
+	public ResponseEntity<List<Book>>  getAllBook() throws Exception{
+		List<Book> books = bookService.getBooks();
+		if(books.isEmpty()) return new ResponseEntity(HttpStatus.NO_CONTENT);
+		else return new ResponseEntity(books, HttpStatus.OK);
+		
+	}
+
+	//특정한 isbn에 해당하는 책 1권을 받아오는 요청을 작성
+	//http://127.0.0.1:8888/rest/api/book/1233-111-111
+	@GetMapping("book/{isbn}")
+	public ResponseEntity<Book> getBook(@PathVariable String isbn) throws Exception{
+		Book book = bookService.searchByIsbn(isbn);
+		if (book==null) return new ResponseEntity(HttpStatus.NO_CONTENT);
+		else return new ResponseEntity(book, HttpStatus.OK);
+	}
+	
+	//특정한 책 등록
+	@PostMapping("book")
+	public ResponseEntity<Book> insertBook(@RequestBody Book book) throws Exception{
+		boolean check = bookService.insertBook(book);
+		if(!check) return new ResponseEntity(HttpStatus.NO_CONTENT);
+		else return new ResponseEntity(book, HttpStatus.OK);
+	}
+	
+	@PutMapping("book")
+	public ResponseEntity<Book> updateBook(@RequestBody Book book)throws Exception{
+		boolean check=bookService.update(book);
+		if(!check) return new ResponseEntity(HttpStatus.NO_CONTENT);
+		else return new ResponseEntity(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("book/{isbn}")
+	public ResponseEntity<List<Book>> deleteBook(@PathVariable String isbn) throws Exception{
+		boolean check = bookService.delete(isbn);
+		if(!check) return new ResponseEntity<List<Book>>(HttpStatus.NO_CONTENT);
+		else return new ResponseEntity(HttpStatus.OK);
+	}
+	
+}
